85562a29c453e1d064380cce1729b009ccde79c9
Ajit Pawar <141655345+AjitPawar01@users.noreply.github.com>
2025-02-11 10:06:24 +0530

    Update hello1.py
    Added all Arithmetic operations

--- hello1.py
+++ hello1.py
@@ -1,10 +1,14 @@
 print("Ajit Pawar")
 
-num1=99
-num2=101
+num1=int(input("Enter first number:- "))
+num2=int(input("Enter Second number:- "))
 
 add=num1+num2
 sub=num1-num2
+mul=num1*num2
+div=num1/num2
 
 print("Addition is:", add)
 print("Subtraction is:", sub)
+print("Multiplicationtion is:", mul)
+print("Divide is:", div)
