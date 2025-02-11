253854168a3998e447774e9d2017f04ebb39807e
siddique <siddiue124@gmail.com>
2025-02-11 10:32:19 +0530

    vv

---
+++ haalo.py
@@ -0,0 +1,11 @@
+print("Ajit Pawar")
+
+num1=int(input("Enter first number:- "))
+num2=int(input("Enter Second number:- "))
+
+add=num1+num2
+sub=num1-num2
+
+print("Addition is:", add)
+print("Subtraction is:", sub)
+
